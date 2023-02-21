ec89b37b301ca39efb3035f4f18c23ea081c8cf5
Cameron Griffin <cambackcountry@gmail.com>
2023-02-21 09:49:10 -0800

    getting pictures working

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ let resultList = document.getElementById('results-container');
 
 function Product(name, fileExtension = 'jpg'){
   this.name = name;
-  this.imagePath = `assets/${name}.${fileExtension}`;
+  this.image = `assets/${name}.${fileExtension}`;
   this.timesShown = 0;
   this.votes = 0;
 }
@@ -27,33 +27,28 @@ function Product(name, fileExtension = 'jpg'){
 //Shows 3 functions and makes sure they're not the same//
 function renderImg(){
   let imgOneIndex = randomIndex();
-  let imgTwo = randomIndex();
-  let imgThree =randomIndex();
-}
+  let imgTwoIndex = randomIndex();
+  let imgThreeIndex =randomIndex();
 
-while (true) {
-  imgOneIndex = randomIndex();
-  imgTwoIndex = randomIndex();
-  imgThreeIndex = randomIndex();
 
-  if (imgOneIndex !== imgTwoIndex && imgOneIndex !== imgThreeIndex && imgTwoIndex !== imgThreeIndex) {
-    break; // All three indexes are different, exit the loop
+  while (imgOneIndex === imgTwoIndex || imgOneIndex === imgThreeIndex || imgTwoIndex === imgThreeIndex){
+    imgTwoIndex = randomIndex();
+    imgThreeIndex = randomIndex();
   }
+      imgOne.src = productArray[imgOneIndex].image;
+      imgOne.title = productArray[imgOneIndex].name;
+      imgOne.alt = `This is an image of ${productArray[imgOneIndex].name}`;
+      imgTwo.src = productArray[imgTwoIndex].image;
+      imgTwo.title = productArray[imgTwoIndex].name;
+      imgTwo.alt = `This is an image of ${productArray[imgTwoIndex].name}`;
+      imgThree.src = productArray[imgThreeIndex].image;
+      imgThree.title = productArray[imgThreeIndex].name;
+      imgThree.alt = `This is an image of ${productArray[imgThreeIndex].name}`;
 
-imgOne.src = productArray[imgOneIndex].imagePath;
-imgOne.title = productArray[imgOneIndex].name;
-imgOne.alt = `This is an image of ${productArray[imgOneIndex].name}`;
-imgTwo.src = productArray[imgTwoIndex].imagePath;
-imgTwo.title = productArray[imgTwoIndex].name;
-imgTwo.alt = `This is an image of ${productArray[imgTwoIndex].name}`;
-imgThree.src = productArray[imgThreeIndex].imagePath;
-imgThree.title = productArray[imgThreeIndex].name;
-imgThree.alt = `This is an image of ${productArray[imgThreeIndex].name}`;
-
-productArray[imgOneIndex].views++;
-productArray[imgTwoIndex].views++;
-productArray[imgThreeIndex].views++;
-}
+      productArray[imgOneIndex].views++;
+      productArray[imgTwoIndex].views++;
+      productArray[imgThreeIndex].views++;
+  }
 
 function randomIndex(){
   return Math.floor(Math.random() * productArray.length);
@@ -93,25 +88,25 @@ function ShowResults(){
 
 //Executable Code
 
-let bagItem = new Item('bag');
-let bananaItem = new Item('banana');
-let bathroomItem = new Item('bathroom');
-let bootsItem = new Item('boots');
-let breakfastItem = new Item('breakfast');
-let bubblegumItem = new Item('bubblegum');
-let chairItem = new Item('chair');
-let cthulhuItem = new Item('cthulhu');
-let dogduckItem = new Item('dog-duck');
-let dragonItem = new Item('dragon');
-let penItem = new Item('pen');
-let petsweepItem = new Item('pet-sweep');
-let scissorsItem = new Item('scissors');
-let sharkItem = new Item('shark');
-let sweepItem = new Item('sweep', 'png');
-let tauntaunItem = new Item('tauntaun');
-let unicornItem = new Item('unicorn');
-let watercanItem = new Item('water-can');
-let wineglassItem = new Item('wine-glass');
+let bagItem = new Product('bag');
+let bananaItem = new Product('banana');
+let bathroomItem = new Product('bathroom');
+let bootsItem = new Product('boots');
+let breakfastItem = new Product('breakfast');
+let bubblegumItem = new Product('bubblegum');
+let chairItem = new Product('chair');
+let cthulhuItem = new Product('cthulhu');
+let dogduckItem = new Product('dog-duck');
+let dragonItem = new Product('dragon');
+let penItem = new Product('pen');
+let petsweepItem = new Product('pet-sweep');
+let scissorsItem = new Product('scissors');
+let sharkItem = new Product('shark');
+let sweepItem = new Product('sweep', 'png');
+let tauntaunItem = new Product('tauntaun');
+let unicornItem = new Product('unicorn');
+let watercanItem = new Product('water-can');
+let wineglassItem = new Product('wine-glass');
 
 productArray.push(bagItem,bananaItem,bathroomItem,bootsItem,breakfastItem,bubblegumItem,chairItem,cthulhuItem,dogduckItem,dragonItem,penItem,petsweepItem,scissorsItem,sharkItem,sweepItem,tauntaunItem,unicornItem,watercanItem,wineglassItem);
 
